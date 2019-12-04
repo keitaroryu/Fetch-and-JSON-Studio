cab1834a95c0e6ea4e444b601071db4f9fb8a575
keitaroryu <keitaroryu@gmail.com>
2019-12-03 23:16:06 -0600

    Inserted Code to pull JSON data.  Added sorting function to sort astronaut array by most to least hours in space.

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            
+            function sortAstronautByHoursInSpace(JSON){
+                let astronautsArray = json;
+                let sortedAstronautArray = [];
+                let inserted;
+                for(let i=0; i<astronautsArray.length; i++){
+                    inserted = false;
+                    for(j=0; j<sortedAstronautArray.length; j++){
+                        if(astronautsArray[i].hoursInSpace > sortedAstronautArray[j].hoursInSpace){
+                            inserted = true;
+                            sortedAstronautArray.splice(j, 0, astronautsArray[i]);
+                            break;
+                        }
+                    }
+                    if(!inserted){
+                        sortedAstronautArray.push(astronautsArray[i]);
+                    }
+                }
+                return sortedAstronautArray;
+            }
+
+            let sortedByHoursAstronautArray = sortAstronautByHoursInSpace(json);
+
+            console.log(sortedByHoursAstronautArray);
+
+
+        });
+    });
+});
